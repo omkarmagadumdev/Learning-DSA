f064d07b9ae2633172e9285fba97d39f9dfaf02f
omkarmagadum-dev <omkar.magadum.dev@gmail.com>
2025-06-22 19:22:41 +0530

    namaste dsa

---
+++ index.js
@@ -0,0 +1,26 @@
+// function addtwonumber(a,b){
+//     let add = a*b;
+//     console.log(add)
+// }
+//  let x = 10
+//  let y = 10
+
+// addtwonumber(1,1);
+// addtwonumber(x,y);
+
+
+
+function sum(a,b){
+    let add = a +b ;
+    console.log(add)
+
+
+
+}
+
+
+let x = 10 ;
+let y = 10;
+
+
+sum(x,y)
