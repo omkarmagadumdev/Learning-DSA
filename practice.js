b762713861f0c080f0efdcb04d0884f0450fc0fb
Omkar Magadum <omkar.magadum.dev@gmail.com>
2025-11-05 19:43:03 +0530

    docs: add detailed explanation post on JavaScript lexical environment, scope chain, blocks, closures, and garbage collection

--- practice.js
+++ practice.js
@@ -34,23 +34,58 @@
 // outer()()
 
 
-function outer(){
-    var a = 0;
-   return  function x(){
-        ++a
+// function outer(){
+//     var a = 0;
+//    return  function x(){
+//         ++a
+//         console.log(a)
+//     }
+// }
+
+// var x = outer();
+// x();
+// x();
+// x();
+// x();
+
+// var y = outer();
+// y();
+
+// scalable for the increment and decrement in clousers ft constructer function 
+
+
+// function Outer(){
+//     var a = 0;
+//     this.increment =  function (){
+//         ++a
+//         console.log(a)
+//     }
+//     this.decrement =  function (){
+//         --a
+//         console.log(a)
+//     }
+  
+// }
+
+// var x = new Outer();
+
+// x.increment();
+// x.increment();
+// x.decrement();
+
+
+
+//smartly garbage collected 
+
+function Outer(){
+    var a = 0, b= 10;
+    return function inner(){
         console.log(a)
     }
 }
-var y = outer();
-console.log(y);
 
-y();
-y();
-y();
-y();
-
-
-
+var z = Outer();
+z();
 
 
 
